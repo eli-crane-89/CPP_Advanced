e078d05abc4e8643c7896c799b769038f23cbfd1
EliC <elijacobcrane@gmail.com>
2018-04-16 22:49:47 -0400

    With Lesson 5 complete

--- EliCrane Level 5 HW Submission/5.10/Exercise5/Exercise5/AdditionalFunctions.hpp
+++ EliCrane Level 5 HW Submission/5.10/Exercise5/Exercise5/AdditionalFunctions.hpp
@@ -48,9 +48,9 @@ Result find_if_sequential_greater(const Container<T, TAlloc>& container, value_t
 	auto bind_lte = std::bind(func_lte, _1, x);
 
 	//Define iterators to store values
-	Container::const_iterator it_first = container.begin();
-	Container::const_iterator it_second;
-	Container::const_iterator it_end = container.end();
+	auto it_first = container.begin();
+	auto it_second = it_first;
+	auto it_end = container.end();
 
 	while (it_first != it_end) {
 		it_first = std::find_if(it_first, it_end, bind_lte);
@@ -78,172 +78,172 @@ Result find_if_sequential_greater(const Container<T, TAlloc>& container, value_t
 }
 
 
-////Lower bound
-//
-////Predictate 
-//bool lbPred(const double d1, const double d2) {
-//	return d1 <= d2;
-//}
-//
-//template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
-//Result lower_bound_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
-//
-//	//const iterators to store values
-//	Container::const_iterator it_first = container.begin();
-//	Container::const_iterator it_end = container.end();
-//
-//	while (it_first != it_end) {
-//		//searching for index + 1
-//		//find elem that is greater than or equal to x
-//		//Use Predicate to check if the value is <= before executing algorithm
-//		it_first = std::lower_bound(it_first, it_end, x, lbPred);
-//
-//		if (it_first != it_end) {
-//			//Make sure term is not beginning term
-//			if (it_first != container.begin()) {
-//
-//				//Return distance
-//				int index_first = std::distance(container.begin(), it_first);
-//
-//				return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
-//			}
-//			//Increment it_first
-//			it_first++;
-//		}
-//
-//	}
-//
-//	return std::make_tuple(std::make_tuple(999, 999), false);
-//}
-//
-////Upper bound
-//
-////Predictate 
-//bool ubPred(const double d1, const double d2) {
-//	return d1 < d2;
-//}
-//
-//template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
-//Result upper_bound_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
-//
-//	//const iterators to store values
-//	Container::const_iterator it_first = container.begin();
-//	Container::const_iterator it_end = container.end();
-//
-//	while (it_first != it_end) {
-//		//searching for index + 1
-//		//find elem that is greater than or equal to x
-//		//Use Predicate to check if the value is > x before executing algorithm
-//		it_first = std::upper_bound(it_first, it_end, x, ubPred);
-//
-//		if (it_first != it_end) {
-//			//Make sure term is not beginning term
-//			if (it_first != container.begin()) {
-//
-//				//Return distance
-//				int index_first = std::distance(container.begin(), it_first);
-//
-//				return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
-//			}
-//			//Increment it_first
-//			it_first++;
-//		}
-//
-//	}
-//
-//	return std::make_tuple(std::make_tuple(999, 999), false);
-//}
-//
-////Using Equal_Range
-//
-////Predictate 
-//bool erPred(const double d1, const double d2) {
-//	return d1 < d2;
-//}
-//
-//template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
-//Result equal_range_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
-//
-//	//const iterators to store values
-//	Container::const_iterator it_first = container.begin();
-//	Container::const_iterator it_end = container.end();
-//
-//	while (it_first != it_end) {
-//		//find elem that is greater than or equal to x
-//		//Use Predicate to check if the value is <= before executing algorithm
-//		auto it_pair = std::equal_range(it_first, it_end, x, erPred);
-//
-//		if (it_pair.first != it_end) {
-//
-//			//If .first != .second, that signifies .first is the first element and index has been found
-//			if (it_pair.first != it_pair.second) {
-//				//Return distance
-//				int index_first = std::distance(container.begin(), it_pair.first);
-//
-//				return std::make_tuple(std::make_tuple(index_first, index_first + 1), true);
-//			}
-//			//If it_pair.first == it_pair.second that signifies that no equal term was found, but a higher one was
-//			else if (it_pair.first == it_pair.second) {
-//				if (it_pair.first == container.begin()) {
-//					//Increment if it_pair.first is first term and continue with loop
-//					it_first++;
-//				}
-//				else {
-//					//else the index + 1 has been found
-//
-//					//Return distance
-//					int index_first = std::distance(container.begin(), it_pair.first);
-//
-//					return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
-//				}
-//			}
-//
-//		}
-//		else {
-//			//Trigger exit condition
-//			it_first = it_end;
-//		}
-//	}
-//
-//	return std::make_tuple(std::make_tuple(999, 999), false);
-//}
-//
-////Find if Not
-//
-//bool IsE(const double i, const double num) {
-//
-//	if (i == num) {
-//		return true;
-//	}
-//
-//	return false;
-//}
-//
-//template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
-//int find_if_not_equal(const Container<T, TAlloc>& container, value_type x) {
-//
-//	using namespace std::placeholders;
-//
-//	//Bind Is Equal Function
-//	intFunc func_e = IsE;
-//	auto bind_e = std::bind(func_e, _1, x);
-//
-//	//Define iterators to store values
-//	Container::const_iterator it_first = container.begin();
-//	Container::const_iterator it_end = container.end();
-//
-//
-//	it_first = std::find_if_not(it_first, it_end, bind_e);
-//
-//	//if it_first is found continue
-//	if (it_first != it_end) {
-//
-//		//Return distance
-//		return std::distance(container.begin(), it_first);
-//	}
-//
-//	return 999;
-//
-//}
+//Lower bound
+
+//Predictate 
+bool lbPred(const double d1, const double d2) {
+	return d1 <= d2;
+}
+
+template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
+Result lower_bound_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
+
+	//const iterators to store values
+	auto it_first = container.begin();
+	auto it_end = container.end();
+
+	while (it_first != it_end) {
+		//searching for index + 1
+		//find elem that is greater than or equal to x
+		//Use Predicate to check if the value is <= before executing algorithm
+		it_first = std::lower_bound(it_first, it_end, x, lbPred);
+
+		if (it_first != it_end) {
+			//Make sure term is not beginning term
+			if (it_first != container.begin()) {
+
+				//Return distance
+				int index_first = std::distance(container.begin(), it_first);
+
+				return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
+			}
+			//Increment it_first
+			it_first++;
+		}
+
+	}
+
+	return std::make_tuple(std::make_tuple(999, 999), false);
+}
+
+//Upper bound
+
+//Predictate 
+bool ubPred(const double d1, const double d2) {
+	return d1 < d2;
+}
+
+template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
+Result upper_bound_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
+
+	//const iterators to store values
+	auto it_first = container.begin();
+	auto it_end = container.end();
+
+	while (it_first != it_end) {
+		//searching for index + 1
+		//find elem that is greater than or equal to x
+		//Use Predicate to check if the value is > x before executing algorithm
+		it_first = std::upper_bound(it_first, it_end, x, ubPred);
+
+		if (it_first != it_end) {
+			//Make sure term is not beginning term
+			if (it_first != container.begin()) {
+
+				//Return distance
+				int index_first = std::distance(container.begin(), it_first);
+
+				return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
+			}
+			//Increment it_first
+			it_first++;
+		}
+
+	}
+
+	return std::make_tuple(std::make_tuple(999, 999), false);
+}
+
+//Using Equal_Range
+
+//Predictate 
+bool erPred(const double d1, const double d2) {
+	return d1 < d2;
+}
+
+template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
+Result equal_range_sequential_greater(const Container<T, TAlloc>& container, value_type x) {
+
+	//const iterators to store values
+	auto it_first = container.begin();
+	auto it_end = container.end();
+
+	while (it_first != it_end) {
+		//find elem that is greater than or equal to x
+		//Use Predicate to check if the value is <= before executing algorithm
+		auto it_pair = std::equal_range(it_first, it_end, x, erPred);
+
+		if (it_pair.first != it_end) {
+
+			//If .first != .second, that signifies .first is the first element and index has been found
+			if (it_pair.first != it_pair.second) {
+				//Return distance
+				int index_first = std::distance(container.begin(), it_pair.first);
+
+				return std::make_tuple(std::make_tuple(index_first, index_first + 1), true);
+			}
+			//If it_pair.first == it_pair.second that signifies that no equal term was found, but a higher one was
+			else if (it_pair.first == it_pair.second) {
+				if (it_pair.first == container.begin()) {
+					//Increment if it_pair.first is first term and continue with loop
+					it_first++;
+				}
+				else {
+					//else the index + 1 has been found
+
+					//Return distance
+					int index_first = std::distance(container.begin(), it_pair.first);
+
+					return std::make_tuple(std::make_tuple(index_first - 1, index_first), true);
+				}
+			}
+
+		}
+		else {
+			//Trigger exit condition
+			it_first = it_end;
+		}
+	}
+
+	return std::make_tuple(std::make_tuple(999, 999), false);
+}
+
+//Find if Not
+
+bool IsE(const double i, const double num) {
+
+	if (i == num) {
+		return true;
+	}
+
+	return false;
+}
+
+template <typename T, template <typename S, typename Alloc> class Container, typename TAlloc>
+int find_if_not_equal(const Container<T, TAlloc>& container, value_type x) {
+
+	using namespace std::placeholders;
+
+	//Bind Is Equal Function
+	intFunc func_e = IsE;
+	auto bind_e = std::bind(func_e, _1, x);
+
+	//Define iterators to store values
+	auto it_first = container.begin();
+	auto it_end = container.end();
+
+
+	it_first = std::find_if_not(it_first, it_end, bind_e);
+
+	//if it_first is found continue
+	if (it_first != it_end) {
+
+		//Return distance
+		return std::distance(container.begin(), it_first);
+	}
+
+	return 999;
+
+}
 
 #endif
